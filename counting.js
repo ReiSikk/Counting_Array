5818a9d3e7ee16533c9ae6d204aa852f27204217
Rei Sikk <reix0010@stud.kea.dk>
2022-08-31 12:30:15 +0200

    counter done

---
+++ counting.js
@@ -0,0 +1,39 @@
+/* const people = ["Harry", "ron", "Hermione"];
+let result;
+
+result = people.push("Draco");
+result = people.pop();
+result = people.push("Nevillle");
+result = people.push("Luna");
+result = people.slice(0, 3);
+result = people.splice(1, 0, "Cho");
+people[1] = "Ginny";
+result = people.push("Fred", "George");
+result = people.indexOf("Fred");
+result = people.splice(result, 1);
+console.log(result);
+console.log(people);
+ */
+
+const countingArr = [0];
+const numberOfLoops = 9;
+let i = 0;
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  console.log("init");
+  counter();
+}
+
+function counter() {
+  i++;
+  if (countingArr.length) {
+    setTimeout(counter, 900);
+    countingArr.unshift(i);
+    countingArr.length = numberOfLoops;
+    console.log(countingArr);
+  } else {
+    console.log("else");
+  }
+}
